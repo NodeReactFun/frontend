ede41eba38bd790667dc43d7273696e2243dff08
kralinc <lelinxi@protonmail.com>
2021-07-22 10:25:23 -0500

    moved name and description from being hardcoded to being props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Header from './components/Header'
 import MediaCard from './components/MediaCard'
 
 function App() {
+  const placeholderName = "Lizard";
+  const placeholderDesc = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica";
   return (
     <div className="App">
         <Header />
         <div>
-          <MediaCard />
-          <MediaCard />
-          <MediaCard />
+          <MediaCard name={placeholderName} desc={placeholderDesc}/>
+          <MediaCard name={placeholderName} desc={placeholderDesc}/>
+          <MediaCard name={placeholderName} desc={placeholderDesc}/>
         </div>
     </div>
   );
